21ecbe57f8fa51ee543019fd59c6241c828cfa51
jm4293 <dlwoals4293@gmail.com>
2022-12-06 16:02:13 +0900

    mac commit - 서버 합치기

---
+++ server/server.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const app = express();
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '123456789',
+    database: 'express_db'
+})
+connection.connect();
+
+const cors = require('cors');
+app.use(cors());
+
+const bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+/////////////////
+// login - 3001//
+/////////////////
+app.post('/userInfoUpdate', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const sendText = {
+        email: email,
+        password: password
+    };
+
+    connection.query('INSERT INTO user_info (user_email, user_password) VALUES(?, ?)', [email, password], (err, rows, fields) => {
+        if (err) {
+            console.log("로그인 서버 - 회원가입 성공");
+            console.log(err);
+        }
+        else {
+            console.log("로그인 서버 - 회원가입 완료")
+            res.send(sendText);
+        }
+    })
+})
+
+app.post('/userInfoRead', (req, res) => {
+    connection.query('SELECT * FROM user_info', (err, rows, fields) => {
+        if (err) {
+            console.log("로그인 서버 - 이메일, 비민번호 찾기 실패");
+            console.log(err);
+        }
+        else {
+            console.log("로그인 서버 - 이메일, 비민번호 찾기 성공");
+            res.send(rows);
+        }
+    })
+})
+
+app.listen(3001, () => {
+    console.log(`로그인 서버: 3001`);
+})
+
+////////////////////
+// chatting - 3002//
+////////////////////
+var http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+    cors: {
+        // origin: "*"
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    socket.on("send message", (item) => {
+        const message = "닉네임: " + item.name + " 내용: " + item.msg;
+        console.log(`채팅 서버 - 보낸 메세지: ${message}`);
+        io.emit("receive message", { name: item.name, msg: item.msg });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("채팅 서버 - 유저 종료: ", socket.id)
+    });
+
+    socket.on('error', (error) => {
+        console.log(`채팅 서버 - 에러 발생: ${error}`);
+    })
+});
+
+http.listen(3002, () => {
+    console.log(`채팅 서버: 3002`);
+});
+
+
+////////////////////////
+// noticeboard - 3003 //
+////////////////////////
+app.post('/board', (req, res) => {
+    connection.query('SELECT * FROM noticeboard', (err, rows, fields) => {
+        if (err) {
+            console.log("게시판 서버 - 게시판 불러오기 실패");
+            console.log(err);
+        }
+        else {
+            console.log("게시판 서버 - 게시판 불러오기 성공");
+            res.send(rows);
+        }
+    });
+});
+
+app.post('/board/register', (req, res) => {
+    const title = req.body.title;
+    const writer = req.body.writer;
+    const content = req.body.content;
+    const date = req.body.date;
+
+    connection.query('INSERT INTO noticeboard (title, writer, content, date) VALUES(?, ?, ?, ?)', [title, writer, content, date], (err, rows, fields) => {
+        if (err) {
+            console.log("게시판 서버 - 게시판 등록 실패");
+            console.log(err);
+        } else {
+            console.log("게시판 서버 - 게시판 등록 성공")
+            res.send(rows);
+        }
+    });
+});
+
+app.post('/board/view/comment', (req, res) => {
+
+});
+
+app.listen(3003, () => {
+    console.log(`게시판 서버: 3003`);
+});
